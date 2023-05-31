ab96db7bf09faee9e4909b62fc3555e2ca286f0e
kmin0716 <95474630+kmin0716@users.noreply.github.com>
2023-05-31 19:32:21 +0200

    Update reseau.php

--- reseau.php
+++ reseau.php
@@ -7,30 +7,102 @@ include("BDDconnexion.php");
 */
 
 //il faut remplacer par: login et avoir l'ID de l'utilisateur 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $correct = 1;
-    /* if (!isset($_POST["ID_user1"]) || empty($_POST["ID_user1"])) { 
-      echo "Erreur ID_user1<br>";
-      $correct = 0;
-    }
-    */
-    
+ session_start();
+if (!isset($_SESSION["userID"]) || empty($_SESSION["userID"]) || $_SESSION["userAdmin"] != 1) {
+  echo "<script>alert('Connecte toi avant');document.location.replace('login.php');</script>";
+  die();
 }
+$userID = $_SESSION["userID"];  
+?>
 
-if($correct){
-    $pseudo = $_POST["Pseudo"];
-    $nom = $_POST["Nom"];
-    $emploi = $_POST["emploi"];
+<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js">
+    </script>   
+    <script type="text/javascript">
+    </script>
+</head>
+<body>
+
+<?php
+if($db_found){
+    
     $ami = "SELECT * FROM utilisateur as util1, ami, utilisateur as util2 
             WHERE ((util1.ID=ami.ID_user1 AND util2.ID=ami.ID_user2) 
-            OR (util1.ID=ami.ID_user2 AND util2.ID=ami.ID_user1)) AND util1.ID=1;"
+            OR (util1.ID=ami.ID_user2 AND util2.ID=ami.ID_user1)) AND util1.ID=$userID;";
+    $resulta = mysqli_query($db_handle, $ami);
 
-$info = "SELECT util2.* FROM utilisateur as util1, ami, utilisateur as util2 
+    //Affichage 1
+    echo "<h1>Mon réseau 1</h1><br>";
+    echo "<table border=\"1\">";
+    echo "<tr>";
+    echo "<th>" . "PDP" . "</th>";
+    echo "<th>" . "Nom" . "</th>";
+    echo "<th>" . "Emploi" . "</th>";
+    echo "</tr>";
+
+    while ($data = mysqli_fetch_assoc($resulta)) {
+        echo "<tr>";
+        $PDP = $data['PDP'];
+        echo "<td> <img src='$PDP' height='80' width='100'>" . "</td>";
+        echo "<td>:" . $data['Nom'] . "</td>";
+        echo "<td> " . $data['Emploi'] . "</td>";
+    }
+    echo "</table>";
+
+    $info = "SELECT util2.* FROM utilisateur as util1, ami, utilisateur as util2 
         WHERE ((util1.ID=ami.ID_user1 AND util2.ID=ami.ID_user2) 
-        OR (util1.ID=ami.ID_user2 AND util2.ID=ami.ID_user1)) AND util1.ID=1;"
+        OR (util1.ID=ami.ID_user2 AND util2.ID=ami.ID_user1)) AND util1.ID=$userID;";
+    $resultat = mysqli_query($db_handle, $info);
 }
-//liste des amis: photo des amis + nom + emploi => util2.PDP , util2.Nom, util2.emploi 
-/*imaginons qu'on affiche les amis de toto (son ID = 1) */
+?>
+<!-- il faut la page de l'ami -->
+<a href="vous.html"> 
+  <img src='$PDP' >
+</a>
 
+<?php
+//Affichage 2 
+
+    echo "<div class=\"container\">";
+    echo "<table class=\"table table-striped\">";
+    echo "<tbody>";
+    echo "<tr>";
+    echo "<th>" . "PDP" . "</th>";
+    echo "<th>" . "Nom" . "</th>";
+    echo "<th>" . "Emploi" . "</th>";
+    echo "</tr>";
+    echo "</tbody>";
+
+
+    while ($data = mysqli_fetch_assoc($resultat)) {
+        echo "<tr>";
+        $PDP = $data['PDP'];
+        echo " <td> <img src='$PDP' height='80' width='100'>" . "</td>";
+        echo "<td> " . $data['Nom'] . "</td>";
+        echo "<td> " . $data['Emploi'] . "</td>";
+    }
+    echo "</table>";
+    echo "</div>";
+?>
+
+
+</body>
+</html>
+
+   
+<?php
+ //si le BDD n'existe pas
+ else {
+    echo "Database not found";
+  }
+  mysqli_close($db_handle);
+  
 ?>
 
